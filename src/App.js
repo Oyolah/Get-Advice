74d3c9cef58e6ba74bea9484b62896e8cf751749
oyolah <oyolahmustapha2015@gmail.com>
2020-04-10 19:36:53 +0300

    final commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+
+export default class App extends Component {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1>Advice From Oyolah</h1>
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>Advice Me</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
